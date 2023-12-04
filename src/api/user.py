8257ffcdbc435aaeacc8248373253526595bf38d
Kirill Petryashev <petryashev.k@gmail.com>
2023-12-04 12:10:42 +0300

    feat(model): Добавлен эндпоинт получения идентификатора пользователя по пин-коду

--- src/api/user.py
+++ src/api/user.py
@@ -4,23 +4,38 @@
 
 __author__ = "Kirill Petryashev"
 
-from fastapi import APIRouter, Body, status
+from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import HTTPException
-from src.models.user import User, read_user_by_code
+from src.models.user import User, read_user_id_by_code, read_user_by_id
 
 # Определение роутера для ендпоинтов пользователя
 router = APIRouter(prefix='/user')
 
 
-@router.get("/get", response_model=User)
-async def get_user(code: str = Body(embed=True)) -> JSONResponse:
+@router.get("/get_user_id")
+async def get_user_id_by_code(code: str) -> JSONResponse:
     """
-    Получение данных пользователя по пинкоду
+    Получение идентификатора пользователя по пин-коду
     """
     try:
-        response = read_user_by_code(code)
-        return JSONResponse(content=response.model_dump(),
+        id_ = read_user_id_by_code(code)
+        id_ = "" if id_ is None else id_
+        response = {
+            "id": id_
+        }
+        return JSONResponse(content=response,
                             status_code=status.HTTP_200_OK)
     except ValueError:
-        raise HTTPException(status_code=500, detail="Invalid pincode format")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Неверный формат пин-кода")
+
+
+@router.get("/get", response_model=User)
+async def get_user(id: int | None = None) -> JSONResponse:
+    """
+    Получение данных пользователя по пинкоду или идентификатору
+    """
+    response = read_user_by_id(id)
+    return JSONResponse(content=response.model_dump(),
+                        status_code=status.HTTP_200_OK)

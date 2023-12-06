2239e1cbbab2606679bdd3accdfac75617e1999f
Kirill Petryashev <petryashev.k@gmail.com>
2023-12-06 12:12:51 +0300

    eat(api): Добавлены разрешенные источники для доступа к API

--- src/main.py
+++ src/main.py
@@ -5,9 +5,27 @@
 __author__ = "Kirill Petryashev"
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api import user
 
 app = FastAPI(title="heaven-cash")
+
+# Разрешенные источники
+origins = ["http://127.0.0.1",
+           "http://127.0.0.1:8080",
+           "https://127.0.0.1",
+           "https://127.0.0.1:8080",
+           "http://45.145.6.133",
+           "http://45.145.6.133:8080",
+           "https://45.145.6.133",
+           "https://45.145.6.133:8080"]
+
+# Подключение разрешенных источников
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=['*'],
+                   allow_headers=['*'])
 
 # Подключаем роутеры
 app.include_router(user.router, prefix="/api")

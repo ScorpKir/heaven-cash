7f0f11e15a495ae7be3d2f80a11a6510f043ca52
akiselev123 <kiselyov.id2020@yandex.ru>
2023-12-12 20:32:21 +0300

    feat: realization class CommunalPaymentOperation

--- app/internal/models/operation/operation_types/communal.py
+++ app/internal/models/operation/operation_types/communal.py
@@ -4,11 +4,57 @@
 
 __author__ = "Alexey Kiselev"
 
-from app.internal.models.operation.operation import Operation
-
+from app.internal.models.operation.operation import Operation, OperationTypes
+import app.internal.routes.user as ruser
+from datetime import datetime
 
 class CommunalPaymentOperation(Operation):
     """
     Операция оплаты коммунальных платежей
+
+    user - Идентификатор пользователя, инициировавшего операцию
+    type - Тип операции
+    date - Дата операции
+    amount - Сумма операции
+    additional - Дополнительная информация (номер квитанции)
     """
-    ...
+    def __init__(self, id, user, date, amount, additional):
+        super().__init__(id=id, user=user, date=date, amount=amount,
+                         additional=additional,
+                         type='communal')
+        self.type = 'communal'
+        if additional != None:
+            self.additional = additional
+        else:
+            raise ValueError("additional can not be None!")
+
+    @classmethod
+    def execute(cls):
+        """
+        Выполнение оплаты коммунального платежа
+
+        :return: id пользователя который выполнил операцию
+        :rtype: int
+        """
+
+        # Обработка операции пользователя с указанным идентификатором
+        ruser.user_operation(cls.user, cls.amount, 'communal')
+
+        # Занесение операции в базу данных
+        _id = Operation.create(cls)
+        return _id
+    @classmethod
+    def undo(cls):
+        """
+        Отмена оплаты коммунального платежа
+
+        :return: Логическое значение обозначающее успех операции
+        :rtype: bool
+        """
+
+        # Обработка операции пользователя с указанным идентификатором
+        ruser.user_operation(cls.user, cls.amount, 'communal')
+
+        # Удаление операции из базы данных
+        is_complete = Operation.delete(cls.id)
+        return is_complete

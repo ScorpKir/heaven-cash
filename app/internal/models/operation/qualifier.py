838fcf777fd6976ca460ca18d2d1a1f659d28168
Dmitry Voronin <d.voronin@yarsec.ru>
2023-12-13 14:57:43 +0300

    fix: Поправлено падение PEP8

--- app/internal/models/operation/qualifier.py
+++ app/internal/models/operation/qualifier.py
@@ -8,20 +8,29 @@ from operation import Operation, OperationTypes
 
 
 class OperationTypeQualifier:
+
     @classmethod
     def get_operation(cls, user, operation_type, date, amount, additional):
         if operation_type == OperationTypes.COMMUNAL:
             if additional is None:
                 raise ValueError("NullPointerException")
-            return Operation(user=user, type=operation_type, date=date,
-                             amount=amount, additional=additional)
+            return Operation(user=user,
+                             type=operation_type,
+                             date=date,
+                             amount=amount,
+                             additional=additional)
         elif operation_type == OperationTypes.MOBILE:
             if additional is None:
                 raise ValueError("NullPointerException")
-            return Operation(user=user, type=operation_type, date=date,
-                             amount=amount, additional=additional)
+            return Operation(user=user,
+                             type=operation_type,
+                             date=date,
+                             amount=amount,
+                             additional=additional)
         else:
-            return Operation(user=user, type=operation_type, date=date,
+            return Operation(user=user,
+                             type=operation_type,
+                             date=date,
                              amount=amount)
 
     @classmethod
